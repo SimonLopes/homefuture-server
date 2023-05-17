995b56c32c6b4899edcb6c85c4392a9805fed5b0
Simon Lopes <44950283+SimonLopes@users.noreply.github.com>
2023-05-17 11:24:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+
+
+var server = http.listen(1700, () => {
+  console.log('server is running on port', server.address().port);
+});
